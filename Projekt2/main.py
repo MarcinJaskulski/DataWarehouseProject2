1084b3b8a81e5ffdccb1e75474fd5019bab091db
Paweł Pytel <pawel.pytel@op.pl>
2021-05-20 18:49:54 +0200

    added command line arguments handling

--- Projekt2/main.py
+++ Projekt2/main.py
@@ -3,18 +3,26 @@ import pandas as pd
 from fileReader import read_a_customers, read_a_transactions, read_b_transactions, read_b_customers, read_customer_info
 from createDF import joinAData, joinBData, createDFCustomerInfo
 from createResult import createResultA, createResultB, createResultC
-
+import sys
 # Zmiennne z zadania
-INCOME_THRESHOLD = 20000  # 200000 to za dużo i nic nie zostaje
-TRANSACTION_THRESHOLD = 500
-VIP_INCOME = 10900
+
 
 
 def save_dat_file(data, filename):
     data.to_csv(filename, sep="|", index=False, float_format='%.0f')
 
+def read_arg_from_command_line(arg_index, default_value):
+    try:
+        return int(sys.argv[arg_index])
+    except IndexError:
+        return default_value
 
 if __name__ == '__main__':
+
+    INCOME_THRESHOLD=read_arg_from_command_line(1,20000)
+    TRANSACTION_THRESHOLD = read_arg_from_command_line(2,500)
+    VIP_INCOME = read_arg_from_command_line(3,10900)
+
     a_customers = read_a_customers()
     a_transactions = read_a_transactions()
     b_transactions = read_b_transactions()
@@ -36,7 +44,5 @@ if __name__ == '__main__':
     res_c = createResultC(customer_info_df, INCOME_THRESHOLD, VIP_INCOME)
 
     concat = pd.concat([res_a, res_b, res_c])
+    print(concat.count)
     save_dat_file(concat, "result.dat")
-
-
-
